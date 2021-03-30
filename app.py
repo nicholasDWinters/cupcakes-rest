e770892b2aa80cc08cbe9ae45976298c73508411
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-03-30 14:54:07 -0500

    created cupcake model and set up python files

---
+++ app.py
@@ -0,0 +1,12 @@
+"""Flask app for Cupcakes"""
+from flask import Flask,request,jsonify,render_template
+from models import connect_db, db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
